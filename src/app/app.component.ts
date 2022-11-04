76fe07a3288eebcb4fb4f496fa21aa6a68846418
Pavel Denisov <pv.denisov@interfax.ru>
2022-11-04 09:23:15 +0100

    add barcode settings form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,72 @@
-import {Component, OnInit} from '@angular/core';
-import JsBarcode /* , { Options as jsBarcodeOptions } */ from 'jsbarcode';
+import { Component, OnInit } from '@angular/core';
+import JsBarcode from 'jsbarcode';
+import { FormControl, FormGroup } from '@angular/forms';
+import { defaultEditorColors } from '@taiga-ui/addon-editor';
+
+interface IFormat {
+  readonly code: string;
+  readonly label: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  px2mmFactor?: number;
+  readonly palette = defaultEditorColors;
+  fontList: readonly string[] = [
+    'Arial',
+    'Verdana',
+    'Tahoma',
+    'Trebuchet MS',
+    'Times New Roman',
+    'Georgia',
+    'Garamond',
+    'Courier New',
+    'Brush Script MT',
+  ];
+  formatList: readonly IFormat[] = [
+    { code: 'CODE128', label: 'CODE128 auto' },
+    { code: 'CODE128A', label: 'CODE128 A' },
+    { code: 'CODE128B', label: 'CODE128 B' },
+    { code: 'CODE128C', label: 'CODE128 C' },
+    { code: 'EAN13', label: 'EAN13' },
+    { code: 'EAN8', label: 'EAN8' },
+    { code: 'UPC', label: 'UPC' },
+    { code: 'CODE39', label: 'CODE39' },
+    { code: 'ITF14', label: 'ITF14' },
+    { code: 'ITF', label: 'ITF' },
+    { code: 'MSI', label: 'MSI' },
+    { code: 'MSI10', label: 'MSI10' },
+    { code: 'MSI11', label: 'MSI11' },
+    { code: 'MSI1010', label: 'MSI1010' },
+    { code: 'MSI1110', label: 'MSI1110' },
+    { code: 'pharmacode', label: 'Pharmacode' },
+  ];
+
+  barcodeSetForm = new FormGroup({
+    format: new FormControl('CODE128'),
+    text: new FormControl('test'),
+    height: new FormControl(100),
+    width: new FormControl(2.3),
+    displayValue: new FormControl(false),
+    background: new FormControl('#FFFFFF'),
+    lineColor: new FormControl('#000000'),
+    margin: new FormControl(5),
+    font: new FormControl('monospace'),
+    fontSize: new FormControl(16),
+    textMargin: new FormControl(2),
+    textAlign: new FormControl('right'),
+  });
 
   ngOnInit() {
-    this.px2mmFactor = 1; // this.calcPx2MmFactor();
-
-    let data: string = '230220119012';
-
-    JsBarcode('#barcode', data, {
-      format: 'code128', // default
-      height: 100, // 10mm
-      width: 2.3,
-      // displayValue: false,
-      text: '-' + data + '-',
-      background: 'rgba(0,0,0,0.1)',
-      font: 'monospace',
-      fontOptions: 'bold',
-      fontSize: 16,
-      lineColor: 'darkblue',
-      margin: 5,
-      textMargin: 2,
-      textAlign: 'right',
-      textPosition: 'button',
+    this.barcodeSetForm.valueChanges.subscribe((data) => {
+      this.updateBarcodeImage(data);
     });
+  }
+
+  updateBarcodeImage(data: any) {
+    JsBarcode('#barcode', data.text, data);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,42 @@
-import {TuiRootModule, TuiDialogModule, TuiLoaderModule} from "@taiga-ui/core";
+import {TuiRootModule, TuiDialogModule, TuiLoaderModule, TuiDataListModule, TuiGroupModule} from "@taiga-ui/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {
+  TuiCheckboxLabeledModule,
+  TuiDataListWrapperModule,
+  TuiInputModule,
+  TuiInputSliderModule,
+  TuiRadioBlockModule,
+  TuiSelectModule
+} from "@taiga-ui/kit";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {TuiInputColorModule} from "@taiga-ui/addon-editor";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        TuiRootModule,
-        TuiDialogModule,
-        TuiLoaderModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiLoaderModule,
+    TuiSelectModule,
+    TuiDataListWrapperModule,
+    TuiDataListModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiInputSliderModule,
+    TuiInputColorModule,
+    TuiRadioBlockModule,
+    TuiGroupModule,
+    TuiCheckboxLabeledModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
